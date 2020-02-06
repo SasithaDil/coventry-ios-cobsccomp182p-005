f37749443a5857147c7a5a7b956e7b108786d3b4
Sasitha Dilshan <sasithadilshan77@gmail.com>
2020-02-06 12:24:28 +0530

    Created project file

---
+++ CourseworkIOS/CourseworkIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourseworkIOS
+//
+//  Created by Sasitha Dilshan on 2/6/20.
+//  Copyright © 2020 Sasitha Dilshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
